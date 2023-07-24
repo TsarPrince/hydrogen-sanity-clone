c6c3716516a8664afea3c6a2ca19e04c2ea5836b
Prince Singh <marvelousprince012233@gmail.com>
2023-07-25 01:33:42 +0530

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 
+import Head from 'next/head'
 import NextNProgress from 'nextjs-progressbar'
 
 import Footer from '../components/Footer'
@@ -7,6 +8,19 @@ import Footer from '../components/Footer'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Real Estate & Homes For Sale – Mike Wilen Realty</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="title"
+          content="Real Estate & Homes For Sale – Mike Wilen Realty"
+        />
+        <meta
+          name="description"
+          content="Headed by industry leader Mike Wilen and supported by twelve highly experienced, multidisciplinary professionals. The Group has methodically built and developed a powerhouse real estate practice that sits prominently where buyer’s find real estate for sale. Utilizing our unprecedented resources, we will craft a unique and winning strategy to sell your real estate. Let us put our experience to work for you."
+        />
+        <meta name="author" content="tsarprince" />
+      </Head>
       <NextNProgress color="#3b53ca" />
       <Component {...pageProps} />
       <Footer />
