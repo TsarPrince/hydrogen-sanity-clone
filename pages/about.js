e9123877d8032261fd23470b385a1932dd0b939f
star-works <farukhkhan1115@gmail.com>
2023-08-27 15:29:30 +0530

    try to add link

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,19 @@
 import React from 'react'
-import { createClient } from 'next-sanity'
 import groq from 'groq'
 import imageUrlBuilder from '@sanity/image-url'
 import AboutUs from '../components/about/AboutUs'
 import Navbar from '../components/Navbar'
 import { client } from '../utils/client'
-import Link from 'next/link'
 
 const About = ({ posts }) => {
-  console.log(posts, 'pets')
+  console.log(posts, 'posts')
 
   return (
     <>
       <Navbar />
       <div className="text-red-500 pt-40">
         {posts.length > 0 &&
-          posts.map((post, index) => <BlogPost post={post} />)}
+          posts.map((post, index) => <BlogPost key={index} post={post} />)}
       </div>
       <AboutUs />
     </>
@@ -25,53 +23,124 @@ const About = ({ posts }) => {
 export default About
 
 export function BlogPost({ post }) {
-  // const client = imageUrlBuilder({
-  //   baseUrl: 'https://cdn.sanity.io',
-  //   projectId: 'ww4zhm6z',
-  //   dataset: 'production',
-  // })
-
-  // const urlFor = (source) => builder.image(source)
-  // urlFor(post.mainImage.asset._ref)
-  //   .auto('format')
-  //   .fit('max')
-  //   .width(720)
-  //   .toString()
-
-  // console.log(urlFor)
-
-  // const builder = imageUrlBuilder(config)
-
-  // function urlFor(source) {
-  //   return builder.image(source)
-  // }
-
-  // console.log(urlFor(urlFor(post.mainImage).width(200).url()))
-
-  // const imageBuilder = imageUrlBuilder(client)
   function urlFor(source) {
     return imageUrlBuilder(client).image(source)
   }
 
   return (
     <>
-      <div className="p-4 border rounded-md">
+      <div className="p-8">
         {post.mainImage && (
           <div>
             <img
               className="w-full"
               src={urlFor(post.mainImage.asset._ref).url()}
-              alt={`$s picture`}
+              alt={`${post.title} picture`}
             />
           </div>
         )}
 
-        <h2 className="text-xl font-bold mb-2">{post.title}</h2>
+        <h2 className="about-heading-stylee !font-bold mb-4 lg:mb-8">
+          {post.title}
+        </h2>
         <div className="space-y-2">
           {post.body.map((block, index) => (
-            <p key={index} className="about-heading-stylee mb-4 lg:mb-8">
-              {block.children.map((child) => child.text).join('')}
-            </p>
+            <div key={index} className="about-heading-stylee mb-4 lg:mb-8">
+              {/* Process different block styles */}
+              {block._type === 'block' && (
+                <React.Fragment>
+                  {block.style === 'h2' && <h2>{block.children[0].text}</h2>}
+                  {block.style === 'normal' && (
+                    <p>
+                      {block.children.map((child, childIndex) => {
+                        if (child._type === 'span') {
+                          const isLink = child.marks.some(
+                            (mark) => mark._type === 'link'
+                          )
+
+                          if (isLink) {
+                            const linkMark = child.marks.find(
+                              (mark) => mark._type === 'link'
+                            )
+                            if (linkMark) {
+                              if (linkMark.href.startsWith('mailto:')) {
+                                const mailTo = linkMark.href.substring(7)
+                                return (
+                                  <a
+                                    key={childIndex}
+                                    href={linkMark.href}
+                                    className="text-blue-600 hover:underline"
+                                  >
+                                    {mailTo}
+                                  </a>
+                                )
+                              } else if (linkMark.href.startsWith('tel:')) {
+                                const phoneNumber = linkMark.href.substring(4)
+                                return (
+                                  <a
+                                    key={childIndex}
+                                    href={linkMark.href}
+                                    className="text-green-600 hover:underline"
+                                  >
+                                    {phoneNumber}
+                                  </a>
+                                )
+                              }
+                            }
+                          }
+                        }
+
+                        // If it's plain text
+                        return child.text
+                      })}
+                    </p>
+                        <p>
+                        {block.children.map((child, childIndex) => {
+                          if (child._type === 'span') {
+                            const isLink = child.marks.some(
+                              (mark) => mark._type === 'link'
+                            )
+  
+                            if (isLink) {
+                              const linkMark = child.marks.find(
+                                (mark) => mark._type === 'link'
+                              )
+                              if (linkMark) {
+                                if (linkMark.href.startsWith('mailto:')) {
+                                  const mailTo = linkMark.href.substring(7)
+                                  return (
+                                    <a
+                                      key={childIndex}
+                                      href={linkMark.href}
+                                      className="text-blue-600 hover:underline"
+                                    >
+                                      {mailTo}
+                                    </a>
+                                  )
+                                } else if (linkMark.href.startsWith('tel:')) {
+                                  const phoneNumber = linkMark.href.substring(4)
+                                  return (
+                                    <a
+                                      key={childIndex}
+                                      href={linkMark.href}
+                                      className="text-green-600 hover:underline"
+                                    >
+                                      {phoneNumber}
+                                    </a>
+                                  )
+                                }
+                              }
+                            }
+                          }
+  
+                          // If it's plain text
+                          return child.text
+                        })}
+                      </p>
+                  )}
+                </React.Fragment>
+              )}
+            </div>
           ))}
         </div>
       </div>
@@ -89,19 +158,3 @@ export async function getServerSideProps() {
     },
   }
 }
-
-// export async function getStaticProps() {
-//   const client = createClient({
-//     projectId: 'ww4zhm6z',
-//     dataset: 'production',
-//     useCdn: false,
-//   })
-
-//   const query = `*[_type == "post"]`
-//   const about = await client.fetch(query)
-//   return {
-//     props: {
-//       about,
-//     },
-//   }
-// }
