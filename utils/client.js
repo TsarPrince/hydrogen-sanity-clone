15aa527a9d689c560b73ea2ef90eb3d597e61d40
star-works <farukhkhan1115@gmail.com>
2023-09-05 14:35:26 +0530

    change sanity app

--- utils/client.js
+++ utils/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client'
 
 export const client = createClient({
-  projectId: 'j960yo80',
+  projectId: '727tou9q',
   dataset: 'production',
   useCdn: true, // set to `false` to bypass the edge cache
   // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
