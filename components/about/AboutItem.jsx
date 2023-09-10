4dff25cc5fa967d688ef10a52040f329a68142e5
star-works <farukhkhan1115@gmail.com>
2023-09-11 00:45:33 +0530

    font size and weight

--- components/about/AboutItem.jsx
+++ components/about/AboutItem.jsx
@@ -7,6 +7,7 @@ const AboutItem = ({ post }) => {
   function urlFor(source) {
     return imageUrlBuilder(client).image(source)
   }
+
   return (
     <>
       <div className="p-8">
@@ -20,27 +21,54 @@ const AboutItem = ({ post }) => {
           </div>
         )}
 
-        {post.title === 'Untitled' ? null : (
-          <h2 className="text-black text-[36px] md:text-[44px] leading-[140%] break-words font-montserrat font-bold mb-4 lg:mb-8">
+        {post.title !== 'Untitled' && (
+          <h2
+            className={`text-black ${
+              post.title === 'h1'
+                ? 'text-6xl font-medium'
+                : 'text-[36px] md:text-[44px] font-medium'
+            } ${
+              post.title === 'h1' &&
+              post.body[0]?.children[0]?.marks[0] === 'strong'
+                ? 'font-bold'
+                : ''
+            } leading-[140%] break-words font-montserrat mb-4 lg:mb-8`}
+          >
             {post.title}
           </h2>
         )}
+
         <div>
           {post.body.map((block, index) => (
-            <div
-              key={index}
-              className="font-medium text-black text-[36px] md:text-[44px] leading-[140%] break-words font-montserrat"
-            >
+            <div key={index} className="font-montserrat">
               {block._type === 'block' && (
                 <React.Fragment>
-                  {block.style === 'h2' ||
-                  (block.style === 'h1' &&
-                    block.children[0].marks[0] === 'strong') ? (
-                    <h2 className="mb-4 lg:mb-8 !font-bold">
+                  {block.style === 'h1' && (
+                    <h1
+                      className={`mb-4 lg:mb-8 text-[54px] md:text-[70px]  text-black ${
+                        block.children[0]?.marks[0] === 'strong'
+                          ? 'font-bold'
+                          : 'font-medium'
+                      } `}
+                    >
+                      {block.children[0].text}
+                    </h1>
+                  )}
+                  {block.style === 'h2' && (
+                    <h2
+                      className={`mb-4 lg:mb-8 text-[36px] md:text-[44px]  text-black leading-[140%] break-words ${
+                        block.children[0]?.marks[0] === 'strong'
+                          ? 'font-bold'
+                          : 'font-medium'
+                      } `}
+                    >
                       {block.children[0].text}
                     </h2>
-                  ) : block?.markDefs[0]?._type === 'link' ? null : (
-                    <h2 className="mb-4 lg:mb-8">{block.children[0].text}</h2>
+                  )}
+                  {block.style === 'normal' && (
+                    <p className="mb-4 lg:mb-8 text-black text-[22px] font-normal leading-[140%] break-words">
+                      {block.children[0].text}
+                    </p>
                   )}
                   {block.style === 'normal' && <GenerateLinks block={block} />}
                 </React.Fragment>
