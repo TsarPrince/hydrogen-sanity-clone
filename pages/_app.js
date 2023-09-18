3bf972f2af7063932996d26a4ced538bdb41b116
star-works <farukhkhan1115@gmail.com>
2023-09-18 13:19:02 +0530

    fixed google analytic issue

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,21 @@ import Footer from '../components/Footer'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Head>
         <title>Real Estate & Homes For Sale – Mike Wilen Realty</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -21,16 +36,6 @@ function MyApp({ Component, pageProps }) {
           content="Headed by industry leader Mike Wilen and supported by twelve highly experienced, multidisciplinary professionals. The Group has methodically built and developed a powerhouse real estate practice that sits prominently where buyer’s find real estate for sale. Utilizing our unprecedented resources, we will craft a unique and winning strategy to sell your real estate. Let us put our experience to work for you."
         />
         <meta name="author" content="tsarprince" />
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-679GG8SWEY" />
-        <Script id="google-analytics">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-679GG8SWEY');
-        `}
-        </Script>
       </Head>
       <NextNProgress color="#3b53ca" />
       <Component {...pageProps} />
