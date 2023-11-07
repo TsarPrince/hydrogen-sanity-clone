7445f5d0eb6d570f1d0267159fd4c14f32b1a617
Prince Singh <marvelousprince012233@gmail.com>
2023-11-08 02:57:39 +0530

    search on mobile

--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -22,6 +22,17 @@ const Searchbar = ({ setDialogOpen }) => {
     getSuggestions()
   }, [q])
 
+  const handleOnClick = (e, ListingId) => {
+    e.preventDefault()
+    if (setDialogOpen) {
+      setDialogOpen(false)
+    }
+    router.push({
+      pathname: '/property',
+      query: { id: ListingId },
+    })
+  }
+
   return (
     <div className="relative group z-10">
       {/* Search bar */}
@@ -80,7 +91,10 @@ const Searchbar = ({ setDialogOpen }) => {
 
                     return (
                       <div key={key} className="">
-                        <Link href={`/property?id=${ListingId}`}>
+                        <button
+                          className="w-full"
+                          onClick={(e) => handleOnClick(e, ListingId)}
+                        >
                           <div className="flex items-center hover:bg-gray/40 p-3 rounded space-x-2">
                             <svg
                               className="w-5 m-2 flex-shrink-0"
@@ -89,9 +103,9 @@ const Searchbar = ({ setDialogOpen }) => {
                             >
                               <path d="M19 21.0001H5C4.44772 21.0001 4 20.5524 4 20.0001V11.0001L1 11.0001L11.3273 1.61162C11.7087 1.26488 12.2913 1.26488 12.6727 1.61162L23 11.0001L20 11.0001V20.0001C20 20.5524 19.5523 21.0001 19 21.0001ZM6 19.0001H18V9.15757L12 3.70302L6 9.15757V19.0001Z"></path>
                             </svg>
-                            <p className="">{address}</p>
+                            <p className="text-left">{address}</p>
                           </div>
-                        </Link>
+                        </button>
                       </div>
                     )
                   }
